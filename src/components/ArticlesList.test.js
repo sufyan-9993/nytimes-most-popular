09b2d4d91ef8d3d5e582efeae3d4b9fec926acdc
sufyan-9993 <shaikhsufyan9993@gmail.com>
2024-07-15 23:51:45 +0530

    update

--- src/components/ArticlesList.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ArticlesList from './ArticlesList';
-
-test('renders articles list', async () => {
-  render(<ArticlesList />);
-  const linkElement = await screen.findByText(/Most Popular Articles/i);
-  expect(linkElement).toBeInTheDocument();
-});
